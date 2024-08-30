916abf96304c090eaef7ce944abf5d1426959cbb
Victor Vincent <dev.victorvincent@gmail.com>
2024-08-30 21:46:36 +0100

    0. Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,51 @@
-class RedisClient(){
-	function constructor(){
+import { createClient } from 'redis'
+import {promisify} from 'utils'
+
+class RedisClient{
+	constructor(){
+		this.redisClient = createClient();
+		this.redisClient.on('error', (error) =>{
+		console.log(error)
+		})
+		const getAsync = promisify(this.redisClient.get).bind(this.redisClient);
+		const setAsync = promisify(this.redisClient.set).bind(this.redisClient);
+		const delAsync = promisify(this.redisClient.del).bind(this.redisClient);
+
+	}
+	  isAlive(){
+		return this.redisClient.connected;
 
 
 	}
-	function isAlive(){
 
+	async get(key){
+		try {
+			const value = getAsync(key);
+			return value
+			
+		} catch (error) {
+			console.log(error);
+			throw error;
+		}
+	}
+	async set(key, value, duration){
+		try {
+			await this.setAsync(key, value, 'EX', duration);
+			
+		} catch (error) {
+			console.error(error)
+			throw error;
+		}
 
-
+	}
+	async del(key){
+		try {
+			await this.delAsync(key);
+		} catch (error) {
+			console.error(error)
+		}
 	}
 
 }
+const redisClient = new RedisClient();
+module.exports = redisClient;
